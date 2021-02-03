ee0f31b4e6fc203ae9b43b2bcdcf7c8ca5a41cf4
unknown <volong24071995>
2021-02-03 08:20:28 +0700

    add

---
+++ src/main/java/castudy/dao/DAOHelper.java
@@ -0,0 +1,44 @@
+package castudy.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DAOHelper {
+
+    protected String jdbcURL ="jdbc:postgres://ctlcrsgbiftbfd:f39c225803da667c035af20503834f6f365b913fe1713154135c2ecd7fd8e617@ec2-3-215-118-246.compute-1.amazonaws.com:5432/d19p4k1einmo2f";
+    protected String jdbcUsername = "ctlcrsgbiftbfd";
+    protected String jdbcPassword = "f39c225803da667c035af20503834f6f365b913fe1713154135c2ecd7fd8e617";
+
+
+    protected Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    protected void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+}
